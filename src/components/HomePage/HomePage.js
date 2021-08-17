ba1eb48e8be6908deb3f5015b72c1e8c9bb9e1dd
dwhddnjs <per1215@naver.com>
2021-08-18 00:39:40 +0900

    create new project with finished home/header

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import Header from './../UI/Header/Header'
+import './HomePage.scss'
+
+const HomePage = () => {
+    return (
+        <div className='home-page'>
+        <Header />
+        </div>
+    ) 
+}
+
+export default HomePage
